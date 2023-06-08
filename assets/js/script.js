6802d781563ca8bb31750be8e10146d66bb924d8
Andres Morales <am0173149@gmail.com>
2023-06-08 11:55:17 -0500

    Seccion de habilidades
    Se crea la seccion de habilidades y se le dan las animacion con js

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,3 +20,38 @@ function seleccionar(){
   document.getElementById("nav").classList = "";
   menuVisible = false;
 }
+
+
+/* 
+  Funcion que aplica las animaciones de las habilidades
+ */
+function efectoHabilidades(){
+  var skills = document.getElementById("skills");
+  var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+  
+  if(distancia_skills >= 300){
+      let habilidades = document.getElementsByClassName("progreso");
+      habilidades[0].classList.add("htmlcssbootstrap");
+      habilidades[1].classList.add("php");
+      habilidades[2].classList.add("laravelwordpress");
+      habilidades[3].classList.add("javascript");
+      habilidades[4].classList.add("angularreactjs");
+      habilidades[5].classList.add("mysqlsqlserver");
+      habilidades[6].classList.add("git");
+      habilidades[7].classList.add("autodidacta");
+      habilidades[8].classList.add("kanbanscrum");
+      habilidades[9].classList.add("responsibilidad");
+      habilidades[10].classList.add("trabajoenequipo");
+      habilidades[11].classList.add("facilaprendizaje");
+      habilidades[12].classList.add("creatividad");
+      habilidades[13].classList.add("paqueteriaoffice");
+  }
+}
+
+
+/* 
+  Detecto el scrolling para aplicar la animacion de la barra de habilidades 
+*/
+window.onscroll = function(){
+  efectoHabilidades();
+} 
