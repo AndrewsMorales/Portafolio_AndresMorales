db7317cda991b9e5d5e8e8b40c21468e1460040c
Andres Morales <am0173149@gmail.com>
2023-06-09 09:47:48 -0500

    Seccion Portafolio
    Se crea el apartado del portafolio y se hacen unos cambios y se cargan imagenes.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 /* Inicializacion de variable */
 let menuVisible = false;
+
 /*
   Funcion para ocultar o mostrar el menu dependiendo del caso
 */
@@ -15,43 +16,52 @@ function mostrarOcultarMenu() {
 
 /* 
   Funcion que se encarga de ocultar el menu cuando se selecciona una opcion del menu
-*/ 
-function seleccionar(){
+*/
+function seleccionar() {
   document.getElementById("nav").classList = "";
   menuVisible = false;
 }
 
-
 /* 
   Funcion que aplica las animaciones de las habilidades
  */
-function efectoHabilidades(){
+function efectoHabilidades() {
   var skills = document.getElementById("skills");
-  var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
-  
-  if(distancia_skills >= 300){
-      let habilidades = document.getElementsByClassName("progreso");
-      habilidades[0].classList.add("htmlcssbootstrap");
-      habilidades[1].classList.add("php");
-      habilidades[2].classList.add("laravelwordpress");
-      habilidades[3].classList.add("javascript");
-      habilidades[4].classList.add("angularreactjs");
-      habilidades[5].classList.add("mysqlsqlserver");
-      habilidades[6].classList.add("git");
-      habilidades[7].classList.add("autodidacta");
-      habilidades[8].classList.add("kanbanscrum");
-      habilidades[9].classList.add("responsibilidad");
-      habilidades[10].classList.add("trabajoenequipo");
-      habilidades[11].classList.add("facilaprendizaje");
-      habilidades[12].classList.add("creatividad");
-      habilidades[13].classList.add("paqueteriaoffice");
+  var distancia_skills =
+    window.innerHeight - skills.getBoundingClientRect().top;
+
+  if (distancia_skills >= 300) {
+    let habilidades = document.getElementsByClassName("progreso");
+    habilidades[0].classList.add("htmlcssbootstrap");
+    habilidades[1].classList.add("php");
+    habilidades[2].classList.add("laravelwordpress");
+    habilidades[3].classList.add("javascript");
+    habilidades[4].classList.add("angularreactjs");
+    habilidades[5].classList.add("mysqlsqlserver");
+    habilidades[6].classList.add("git");
+    habilidades[7].classList.add("autodidacta");
+    habilidades[8].classList.add("kanbanscrum");
+    habilidades[9].classList.add("responsibilidad");
+    habilidades[10].classList.add("trabajoenequipo");
+    habilidades[11].classList.add("facilaprendizaje");
+    habilidades[12].classList.add("creatividad");
+    habilidades[13].classList.add("paqueteriaoffice");
   }
 }
-
 
 /* 
   Detecto el scrolling para aplicar la animacion de la barra de habilidades 
 */
-window.onscroll = function(){
+window.onscroll = function () {
   efectoHabilidades();
-} 
+};
+
+/* 
+  Se crea los eventos para cuando precionen algun proyecto los redireccione a un enlace
+*/
+$("#blogResetarios").on("click", function () {
+  window.open("https://github.com/AndrewsMorales/Blog_Resetarios");
+});
+$("#portafolioPersonal").on("click", function () {
+  window.open("https://github.com/AndrewsMorales/Portafolio_AndresMorales");
+});
